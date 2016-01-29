9c35a12d58251f1623f7d341461ac98aa408c88e
Yask Srivastava <yask123@gmail.com>
2016-01-29 19:51:04 +0530

    Fixed issue #6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="redditwallpapers",
-      version="1.4",
+      version="1.5",
       description="Automatically sets the top trending, good resolution images on reddit as your wallpaper.",
       url="https://github.com/yask123/redditwallpapers",
       author="Yask Srivastava",
