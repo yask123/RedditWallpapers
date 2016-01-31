a5d43f242632bd33c00349e191ac9ddc5221b0df
Yask Srivastava <yask123@gmail.com>
2016-01-31 18:00:21 +0530

    Updated version to 1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="redditwallpapers",
-      version="1.7",
+      version="1.8",
       description="Automatically sets the top trending, good resolution images on reddit as your wallpaper.",
       url="https://github.com/yask123/redditwallpapers",
       author="Yask Srivastava",
